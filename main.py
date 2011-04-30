67fd5944f75266527de48407bf65774625512300
Alex <Alex@localhost>
2011-04-29 21:58:46 -0600

    Some really sweet cosmetic improvements on the web interface.

--- main.py
+++ main.py
@@ -4,75 +4,91 @@ from python.needlemanwunsch import NeedlemanWunsch
 from python.load import readfiles
 from subprocess import *
 from buzhug import Base
-import glob, argparse, web
+import glob, argparse, web, json
 
 biocpp = False
 db = []
+results = {}
 #seqs = {} # to be deprecated
 
 ### Bioinformatics
-
-def callbio(seqs, name1, name2):
-	print(seqs)
-	seq1 = seqs[name1] # db here
-	seq2 = seqs[name2]
+class Bio:
+	def __init__(self, algorithm):
+		self.results = {}
+		self.seqs = []
+		self.name = []
+		self.algorithm = algorithm
+		self.error = ""
 	
-	results = {}
+	def addseq(self, seqname):
+		self.name.append(seqname)
 	
-	if biocpp:
-		output = Popen(["./bioinfo", "-nm", seq1, seq2], stdout=PIPE).communicate()[0]
-		# process output into results here
-	else:
-		nw = NeedlemanWunsch(seq1, seq2)
-		nw.align()
-		results['similarity'] = nw.homology()
-		results['alignA'] = nw.A
-		results['alignB'] = nw.B
+	def nw(self):
+		if len(self.name) > 0:
+			seq1 = self.seqs[self.name[0]] # db here
+			seq2 = self.seqs[self.name[1]]
+		elif len(self.name) > 2:
+			self.error = "Too many sequences for Needleman-Wunsch."
+		else:
+			self.error = "Too few sequences for Needleman-Wunsch."
 	
-	return results
+		if biocpp:
+			output = Popen(["./bioinfo", "-nm", seq1, seq2], stdout=PIPE).communicate()[0]
+			# process output into results here
+		else:
+			self.nw = NeedlemanWunsch(seq1, seq2)
+			self.nw.align()
+			self.results['similarity'] = self.nw.homology()
+			self.results['alignA'] = self.nw.A
+			self.results['alignB'] = self.nw.B
+			self.results['diffs'] = self.nw.diffs
 
 ### Database
 
-loading = False
 
-# Define a command-line argument for loading the database.
-'''parser = argparse.ArgumentParser(description='Load sequences into database, one-time only.')
-parser.add_argument('--load', action='store_true')
-args = parser.parse_args()
-print(args)
-if args.load:
-	print(args.load)
-#	db = Base('./db')
-#	loading = True'''
 
 ### Web Interface
 
 #print("Content-type: text/html\n")
 
 urls = (
-	'/', 'main'
-#	'/seq', 'seq'
+	'/', 'main',
+	'/diffs', 'diffs'
 )
 
 render = web.template.render('templates/')
 
+bio = Bio("Needleman-Wunsch")
+bio.addseq("M90848")
+bio.addseq("M90855")
+
 class main:
 	def GET(self):
-		seqs = readfiles()
-		results = callbio(seqs, "M90848", "M90855")
+		bio.seqs = readfiles()
+		if bio.algorithm == "Needleman-Wunsch":
+			bio.nw()
 		#message = "Welcome to bioinfo!\nHere's your output:\n" + out
-		return render.index(results['similarity'], results['alignA'], results['alignB'])
+		if bio.nw.done and bio.error == "":
+			out = render.index( \
+				bio.results['similarity'],
+				bio.name[0],
+				bio.name[1],
+				bio.results['alignA'],
+				bio.results['alignB'],
+				bio.algorithm,
+				len(bio.nw.seq1),
+				len(bio.nw.seq2))
+		else:
+			return bio.error
+		return out
 
-"""
-class seq:
-	def POST():
-		return render.seq(name)
-"""
+class diffs:
+	def POST(self):
+		return json.dumps(bio.results['diffs'])
 
 # Run web app.
 if __name__ == "__main__":
- 	if not loading:
-		app = web.application(urls, globals())
-		app.run()
+	app = web.application(urls, globals())
+	app.run()
 
 #db.close()
