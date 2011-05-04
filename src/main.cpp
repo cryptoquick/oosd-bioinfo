d55a6269e95892e4fe299cf601976fffe0432e50
Alex <Alex@localhost>
2011-05-04 17:54:38 -0600

    Got some JSON goodness happening in C++ now. w00tz

--- src/main.cpp
+++ src/main.cpp
@@ -5,35 +5,34 @@
 //#include <vector>
 //#include "libjson.h"
 #include <iostream>
+#include <sstream>
 #include "ui.h"
 
-//#include "json/reader.h"
-//#include "json/writer.h"
-//#include "json/elements.h"
+#include "../json/reader.h"
+#include "../json/writer.h"
+#include "../json/elements.h"
 
 int main(int argc, char* argv[])
 {
-
-//	UserInterface * ui;
-	// Simple interface for running commands.
-
-	if (argc == 1) {
-	//	ui = new UserInterface(true);
-	//	delete ui;
-	}
-	else if (argc == 3)
+	if (argc == 3)
 	{
-	//	ui = new UserInterface(false);
-		
 		string s1 ("--json");
 		string s2 (argv[1]);
-	//	if (false)
+		
 		if (s1.compare(s2) == 0)
 		{
-			cout << "bla";
-		//	JS * js;
-		//	js = new JS();
-		//	js->test();
+			ostringstream oss (ostringstream::out);
+			
+			json::Object jsobj;
+			jsobj["Name"] = json::String("Schlafly American Pale Ale");
+			jsobj["Origin"] = json::String("St. Louis, MO, USA");
+			jsobj["ABV"] = json::Number(3.8);
+			jsobj["BottleConditioned"] = json::Boolean(true);
+			
+			json::Writer::Write(jsobj, oss);
+			
+			cout << oss.str();
+			
 			// int gap = (int)argv[2];
 			// 		string method = argv[3];
 			// 		string data1 = argv[4];
@@ -50,11 +49,6 @@ int main(int argc, char* argv[])
 	{
 		cout << "incorrect arguments\n";
 	}
-
-	//Just for tests to make sure that the singleton is working as expected
 	
-//	UserInterface * ui2 = new UserInterface();
-//	delete ui2;
-
 	return 0;
 };
