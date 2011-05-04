891b129aaa87591317f90a605dce7a6b7c920bf7
Alex <Alex@localhost>
2011-05-04 17:16:31 -0600

    Minimal main.

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,39 @@
-#include "ui.h"
-//#include "needleman_wunsch.h"
+
+//#include <string>
+//#include <string.h>
+//
 //#include <vector>
-#include <string.h>
-#include "libjson.h"
+//#include "libjson.h"
+#include <iostream>
+#include "ui.h"
+
+//#include "json/reader.h"
+//#include "json/writer.h"
+//#include "json/elements.h"
 
 int main(int argc, char* argv[])
 {
 
-	UserInterface * ui;
+//	UserInterface * ui;
 	// Simple interface for running commands.
 
 	if (argc == 1) {
-		ui = new UserInterface(true);
-		delete ui;
+	//	ui = new UserInterface(true);
+	//	delete ui;
 	}
 	else if (argc == 3)
 	{
-		ui = new UserInterface(false);
+	//	ui = new UserInterface(false);
 		
-		if (strcmp(argv[1], "--json") == 0)
+		string s1 ("--json");
+		string s2 (argv[1]);
+	//	if (false)
+		if (s1.compare(s2) == 0)
 		{
-			JS * js;
-			js = new JS();
-			js->test();
+			cout << "bla";
+		//	JS * js;
+		//	js = new JS();
+		//	js->test();
 			// int gap = (int)argv[2];
 			// 		string method = argv[3];
 			// 		string data1 = argv[4];
@@ -41,10 +52,9 @@ int main(int argc, char* argv[])
 	}
 
 	//Just for tests to make sure that the singleton is working as expected
-	/*
-	UserInterface * ui2 = new UserInterface();
-	delete ui2;
-	*/
+	
+//	UserInterface * ui2 = new UserInterface();
+//	delete ui2;
 
 	return 0;
 };
