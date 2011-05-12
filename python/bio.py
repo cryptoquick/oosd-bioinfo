a2ccc4d1582f9654c83e1a06ef23cd5df9565c63
Alex <Alex@localhost>
2011-05-12 14:43:01 -0600

    Trees and stuff. They're fake, but they look kinda cool.

--- python/bio.py
+++ python/bio.py
@@ -15,7 +15,8 @@ class Bioinformatics:
 		self.error = ""
 		self.cpp = False # Will the program use C++?
 		self.gap = -5
-		self.obj = {}
+		self.obj = {} # For JSON.
+		self.new = "" # Newick format.
 	
 	def addseq(self, seqname):
 		self.name.append(seqname)
@@ -45,11 +46,11 @@ class Bioinformatics:
 		# Use bioinfo binary, or native Python implementation?
 		if self.cpp:
 			self.comm()
-		#	self.results['similarity'] = self.obj["similarity"];
-			self.results['similarity'] = 0.0;
+			self.results['similarity'] = self.obj["similarity"];
+		#	self.results['similarity'] = 0.0;
 			self.results['alignments'] = [self.obj["alignments"][0], self.obj["alignments"][1]]
-		#	self.results['diffs'] = self.obj["diffs"]
-			self.results['diffs'] = []
+			self.results['diffs'] = self.obj["diffs"]
+		#	self.results['diffs'] = []
 		else:
 			self.alg = NeedlemanWunsch(self.seq[0], self.seq[1])
 			self.alg.gap = self.gap
@@ -63,3 +64,31 @@ class Bioinformatics:
 			self.comm()
 			self.results['alignments'] = [sq for sq in self.obj["alignments"]]
 			self.results['tree'] = self.obj["tree"]
+			
+	def newick(self):
+		# FAKE FAKE NEWICK (so fake!)
+		output = ''
+		tree = self.results['tree']
+		tlines = tree.split('\n')
+		
+		output += '('
+		
+		output += '('
+		taxa = tlines[0].split(' ')
+		for taxon in taxa[:-1]:
+			output += str(taxon) + ":0.15" + ','
+		output = output[:-1]
+		output += '):'
+		
+		taxa = tlines[1].split(' ')
+		for taxon in taxa[:-1]:
+			output += str(taxon) + ":0.35" + ','
+		output = output[:-1]
+		output += ')'
+			
+		self.new = output + ':0.2;'
+	#	Know thy enemy:
+	#	S3 S-1 S-1 
+	#	S5 S0 S1 S-1 to:
+	#	(B:6.0,(A:5.0,C:3.0,E:4.0):5.0,D:11.0);
+	#	(S3:1.0,S-1:1.0,S-1:1.0)(S5:1.0,S0:1.0,S1:1.0,S-1:1.0);
